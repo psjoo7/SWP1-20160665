3cca9a1dc59cc185e2396bf98d46801bb1f4780e
psjoo7 <jmin9480@naver.com>
2020-06-26 14:34:47 +0900

    Issue #4 Setting the inital number -1 Used code 'except ValueError:'

--- calc/calc.py
+++ calc/calc.py
@@ -10,6 +10,9 @@ def application(environ, start_response):
 		first_num, second_num = int(first_num), int(second_num)
 		sum = first_num + second_num
 		mul = first_num * second_num
+	except ValueError:
+		sum = -1
+		mul = -1
 	response_body = html % {'sum':sum, 'mul':mul}
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
